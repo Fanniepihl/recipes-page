6250ebfe7ccdc162934478eb5ee81e341db7a67f
nhungvus <nhung@live.se>
2017-10-31 12:56:53 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+   <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+
+    <title>Recept</title>
+</head>
+ <header class="site-header">
+    <nav class="site-nav">
+        <ul>
+            <li> <a href="index.php">Home</a></li>   
+            <li> <a href="favouriteRecipes.php">Favourites</a></li>
+            <li> <a href="#">My recipes</a></li>
+            <li> <a href="#">Contact</a></li>
+        </ul>
+    </nav>
+  </header> 
+     
+    <div id="bread"> 
+     <h1 class="bread-titel">bread.</h1>
+  <div class="wrap">
+  <p class="search-title">What are you feeling for today?</p>
+   <div class="search">
+      <input type="text" class="searchTerm" placeholder="Search muthafuckin' recipes">
+      <button type="submit" class="searchButton">
+        <!--<i class="fa fa-search"></i>-->
+        <img src="img/magnifying.png" class="magnify"/>
+     </button>
+   </div><!---search-title--->
+</div><!--end wrap-->
+
+</div> <!--end bread--> 
